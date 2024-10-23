60c3a0b5f1b4d875272e604d41bb9a4695076e63
omvaddi <147666463+omvaddi@users.noreply.github.com>
2024-10-22 20:35:43 -0400

    redid directories and made login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,13 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import Login from './Login'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import LoginSignup from './Components/LoginSignup/LoginSignup'
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <h1>Home Page</h1>
-      </header>
-      <nav>
-          <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-      </nav>
+    <Router>
       <Routes>
-        <Route path='/register' element={<Signup />}></Route>
-        <Route path='/login' element={<Login />}></Route>
+        <Route path="/login" element={<LoginSignup />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
